53c8bc395fe02134134d659e155c51f2442627a9
TampliteSK <regentfoo.rf@gmail.com>
2025-07-11 21:01:07 +0800

    Safety margin 2 Bench: 1293468

--- src/UciHandler.cpp
+++ src/UciHandler.cpp
@@ -74,7 +74,7 @@ void UciHandler::parse_go(Board* pos, HashTable* table, SearchInfo* info, const 
     // Time Management
     if (time != -1) {
         info->timeset = true;
-        int buffered_time = time - std::max(time / 5, 50);
+        int buffered_time = time - std::min(time / 2, 50);
         double time_allocated = allocate_time(pos, buffered_time);
         info->stop_time = uint64_t(info->start_time + time_allocated + inc / 2);
     }
