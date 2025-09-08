e9ad988b76cbae062581a32ef1275a1800af3e00
TampliteSK <regentfoo.rf@gmail.com>
2025-09-08 21:09:03 +0800

    Search: MDP maybe fixed? Bench: 1375835

--- src/search.cpp
+++ src/search.cpp
@@ -308,7 +308,7 @@ static inline int negamax_alphabeta(Board* pos, HashTable* table, SearchInfo* in
 
 		// Mate distance pruning
 		alpha = std::max(alpha, -MATE_SCORE + (int)pos->ply);
-		beta = std::min(beta, MATE_SCORE - (int)pos->ply - 1);
+		beta = std::min(beta, MATE_SCORE - (int)pos->ply);
 		if (alpha >= beta) {
 			return alpha;
 		}
